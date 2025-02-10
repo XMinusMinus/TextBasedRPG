29cf1c23b5e481d63e4ee2fe89756dd0cf1ec97f
Owen Koebe <ockoebe@gmail.com>
2025-02-10 13:56:04 -0600

    After last Friday
    Update after Friday's work.

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,33 @@
 public class Item {
+    //Basic properties
+    private String name;
+    private int amount;
+    private double weight;
+    private double value;
+    private boolean isConsumable;
+
+    //Default constructor
+    public Item(String name, int amount, double weight, double value, boolean isConsumable) {
+        this.name = name;
+        this.amount = amount;
+        this.weight = weight;
+        this.value = value;
+        this.isConsumable = isConsumable;
+    }
+
+    public int getItemAmount() {
+        return this.amount;
+    }
+
+    public double getItemWeight() {
+        return this.weight;
+    }
+
+    public double getItemValue() {
+        return this.value;
+    }
+
+    public boolean isItemConsumable() {
+        return this.isConsumable;
+    }
 }
--- src/Stats.java
+++ src/Stats.java
@@ -1,2 +1,17 @@
-public class Stats {
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Stats {
+private final Map<String, Double> stats = new HashMap<>();
+
+public void setStat(String statName, double value ) {
+ stats.put(statName, value);
+}
+public double getStat(String statName) {
+    return stats.get(statName);
+}
+public boolean hasStat(String statName){
+    return stats.containsKey(statName);
+}
+
 }
